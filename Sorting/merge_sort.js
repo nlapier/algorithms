240ff19d5cbb4c74cca5e31c9454bf5a6585b001
Nathaniel LaPier <Nat@Nathaniels-Pro.home>
2017-02-11 20:54:38 -0500

    implimented merge sort

--- Sorting/merge_sort.js
+++ Sorting/merge_sort.js
@@ -1,26 +1,23 @@
 function compareAndMerge(arr1, arr2){
 	let output = [];
-	while (arr1.length + arr2.length > 0){
-		if (arr1[0] < arr2[0]){
+
+	while ((arr1.length + arr2.length) > 0){
+		if (arr1[0] < arr2[0] || arr2[0] === undefined){
 			output.push(arr1.shift());
-		}else{
+		} else {
 		  output.push(arr2.shift());
 		}
 	}
 	return output;
 }
 
-
-
 function mergeSort(arr){
+	if (arr.length <= 1){
+		return arr;
+	}
+	const half = Math.ceil((arr.length)/2);
+	const arr1 = arr.slice(0, half);
+	const arr2 = arr.slice(half);
 
-
-
-	if (arr.length)
-
-	const half = Math.floor((arr.length-1)/2)
-
-
-
-
+	return compareAndMerge(mergeSort(arr1), mergeSort(arr2));
 }
