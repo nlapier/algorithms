c006d85f9bfc23db217287c50b71e833c23867ae
Nathaniel LaPier <nathaniel.lapier@gmail.com>
2017-02-18 19:31:41 -0500

    testing implimentation with countingSort

--- Sorting/radix_sort.js
+++ Sorting/radix_sort.js
@@ -1,28 +1,47 @@
 const test =[9,4,233,10,22,100,4000];
 
 function createArray(value, length){
-	let output = []
+	let output = [];
 	for(let i = 1; i <= length; i++){
 		output.push(value);
 	}
 	return output;
 }
 
+function countingSort(arr, base){
+	const len = arr.length;
+	const output = createArray(0, len);
+	const countArray = createArray(0, 10);
+
+	for(let i = 0; i < len; i++){
+		const countIndex = (Math.floor(arr[i]/base))%10;
+		count[countIndex]++;
+	}
+
+	for(let i = 0; i < 10; i++){
+		countArray[i] += countArray[i-1];
+	}
+
+	let i = len-1;
+	while(i>=0){
+		const countIndex = (Math.floor(arr[i]/base))%10;
+		countArray[countIndex]--;
+		const outputIndex = countArray[countIndex];
+		output[outputIndex] = arr[i];
+		i--;
+	}
+
+	return output;
+}
+
 function radixSort(arr){
 	let power = 1;
-	let largestNumberLength = Math.max(arr).toString.length;
-	const len = arr.length;
+	const largestNumberLength = Math.max(arr).toString.length;
+	const output = [...arr];
 
 	while (largestNumberLength/power){
-		const output = createArray(0, len);
-		const countArray = createArray(0, 10);
-
-		for(let i = 0; i < len; i++){
-			const countIndex = Math.floor(arr[i]/power)%10;
-			count[countIndex]++;
-		}
-
-		
+		output = countingSort(output, power);
+		power*=10;
 	}
 
 	return output;
