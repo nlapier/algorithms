25dc9c4200f242941bf4c4440b339150dce56af4
Nathaniel LaPier <Nat@Nathaniels-Pro.home>
2017-02-20 17:05:33 -0500

    add method works

--- Data Structures/binary_tree.js
+++ Data Structures/binary_tree.js
@@ -11,12 +11,13 @@ function Node(value){
 BinaryTree.prototype.add = function(value){
 	const node = new Node(value);
 	let parent = null,
-	childSide = null.
-	currentNode = this.root;
+	childSide = null,
+	currentNode = null;
 
 	if (!this.root){
 		this.root = node;
 	} else {
+	 	currentNode = this.root;
 		while (currentNode){
 			parent = currentNode;
 			if (value > currentNode.value){
@@ -24,11 +25,25 @@ BinaryTree.prototype.add = function(value){
 				childSide= "rightChild";
 			} else {
 				currentNode = currentNode.leftChild;
-				childSide = "leftChild"
+				childSide = "leftChild";
 			}
 		} 
 		parent[childSide] = node;
 	}
 
 	return node;
-}
+};
+
+
+
+
+/*Test area
+const test = new BinaryTree();
+
+test.add(10);
+test.add(8);
+test.add(11);
+test.add(12)
+test.add(9);
+console.log(test)
+*/
