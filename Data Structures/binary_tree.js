a0e164f73d5fc29a2e42b18fcf7ccd0e7d0b8d58
Nathaniel LaPier <Nat@Nathaniels-Pro.home>
2017-02-20 16:59:20 -0500

    testing add method

--- Data Structures/binary_tree.js
+++ Data Structures/binary_tree.js
@@ -1,11 +1,34 @@
 function BinaryTree(){
 	this.root = null;
-
 }
 
-function BinaryTreeNode(value){
+function Node(value){
 	this.value = value;
-	this.parent = null;
 	this.leftChild = null;
 	this.rightChild = null;
+}
+
+BinaryTree.prototype.add = function(value){
+	const node = new Node(value);
+	let parent = null,
+	childSide = null.
+	currentNode = this.root;
+
+	if (!this.root){
+		this.root = node;
+	} else {
+		while (currentNode){
+			parent = currentNode;
+			if (value > currentNode.value){
+				currentNode = currentNode.rightChild;
+				childSide= "rightChild";
+			} else {
+				currentNode = currentNode.leftChild;
+				childSide = "leftChild"
+			}
+		} 
+		parent[childSide] = node;
+	}
+
+	return node;
 }
