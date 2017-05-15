6b471ab49f7f8c8bba1e250f72e39de56b5acbb0
Nathaniel LaPier <Nat@Nathaniels-Pro.home>
2017-05-15 13:47:42 -0400

    added more versions of quickSwitch

--- General/quick_switch.js
+++ General/quick_switch.js
@@ -1,5 +1,17 @@
+//Side-effect version
 function quickSwitch(input, firstIndex, secondIndex){
 	input[secondIndex] = [input[firstIndex], input[firstIndex] = input[secondIndex]][0];
 }
+
+//Pure function version
+function quickSwitch(input, firstIndex, secondIndex){
+	input[secondIndex] = [input[firstIndex], input[firstIndex] = input[secondIndex]][0];
+	return input;
+}
+
+//Array method version
+Array.prototype.quickSwitch = function (firstIndex, secondIndex){
+	this[secondIndex] = [this[firstIndex], this[firstIndex] = this[secondIndex]][0];
+};
 
 //firstElement = [secondElement, secondElement = firstElement][0]
